c32dff64516f7641b4ae32ddc80b1b2de1c47d63
Mads Nyborg <MANY@kea.dk>
2023-12-23 21:55:01 +0100

    test fail

--- src/test/java/com/example/azure/AzureApplicationTests.java
+++ src/test/java/com/example/azure/AzureApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.azure;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class AzureApplicationTests {
 
     @Test
     void contextLoads() {
+        fail();
     }
 
 }
