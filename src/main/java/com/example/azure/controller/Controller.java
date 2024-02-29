e480763633d5ab27273c92431b445315a5352ca8
Mads Nyborg <MANY@kea.dk>
2024-02-29 19:54:17 +0100

    initial commit

--- src/main/java/com/example/azure/controller/Controller.java
+++ src/main/java/com/example/azure/controller/Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/welcome")
     public String Welcome() {
-        return "Velkommen til undervisning i Azure deployment";
+        return "Initial commit";
 
     }
 }
