a6ac22ca3aa90c6715ccfa236160bf13ef7d7a9f
Mads Nyborg <MANY@kea.dk>
2024-03-01 10:09:25 +0100

    Velkomsthilsen opdateret

--- src/main/java/com/example/azure/controller/Controller.java
+++ src/main/java/com/example/azure/controller/Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/welcome")
     public String Welcome() {
-        return "Initial commit";
+        return "Velkommen til Azure";
 
     }
 }
